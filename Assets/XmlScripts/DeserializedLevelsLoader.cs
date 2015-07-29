b69614d3e3ccf02b8b60856c9c72f058259e93d2
Pavel Kvas <kvaspasha@yandex.ru>
2015-07-29 15:05:21 +0300

    refactor DeserializedLevelsLoader

--- Assets/XmlScripts/DeserializedLevelsLoader.cs
+++ Assets/XmlScripts/DeserializedLevelsLoader.cs
@@ -6,125 +6,147 @@ using UnityEngine;
 
 public class DeserializedLevelsLoader
 {
-	// Levels deserialized
-	private DeserializedLevels deserializedLevels;
+    // Levels deserialized
+    private DeserializedLevels deserializedLevels;
 
-	private const string prefabsFolder = "Prefabs/";
-	
-	struct ItemStruct
-	{
-		public GameObject	prefab;
-		public float		x;
-		public float		y;
-		public float		rot;
-		public float		scale_x;
-		public float		scale_y;
-	}
-	
-	// Cache prefabs in prefabDict
-	Dictionary<string,GameObject> prefabPool;
+    private const string prefabsFolder = "Prefabs/";
 
-	// Cache all items with locations
-	List<ItemStruct> sceneItemsList;
+    struct ItemStruct
+    {
+        public GameObject prefab;
+        public float x;
+        public float y;
+        public float rot;
+        public float scale_x;
+        public float scale_y;
+    }
 
-	Transform parentOfXmlItems;
-	
-	public const string xmlItemsGOName = "XmlItems";
+    // Cache prefabs in prefabDict
+    Dictionary<string, GameObject> prefabPool;
 
-	public void generateItems ()
-	{
-		prefabPool = new Dictionary<string, GameObject>();
-		sceneItemsList = new List<ItemStruct>();
+    // Cache all items with locations
+    List<ItemStruct> sceneItemsList;
 
-		// if the XmlItems gameobject folder remained in the Hierarcy, then delete it
-		while (GameObject.Find (xmlItemsGOName) != null)
-			MonoBehaviour.DestroyImmediate(GameObject.Find (xmlItemsGOName));
-		
-		parentOfXmlItems = new GameObject(xmlItemsGOName).transform;
+    Transform parentOfXmlItems;
 
-		deserializedLevels = XmlIO.LoadXml<DeserializedLevels>("Levels");
+    public const string xmlItemsGOName = "XmlItems";
 
-		// if startlevel is in the XML i.e. <Developer StartLevel="3" /> then get level from there
-		// otherwise start with level 1
-		int startLevel = int.Parse (deserializedLevels.developer.startLevel);
+    private void init()
+    {
+        prefabPool = new Dictionary<string, GameObject>();
+        sceneItemsList = new List<ItemStruct>();
 
-		DeserializedLevels.Level currentLevel = deserializedLevels.levels[startLevel-1];
+        // if the XmlItems gameobject folder remained in the Hierarcy, then delete it
+        while (GameObject.Find(xmlItemsGOName) != null)
+            MonoBehaviour.DestroyImmediate(GameObject.Find(xmlItemsGOName));
 
-		// <Item prefab="Chair" x="1" y="10" rot="90" />
-		foreach (DeserializedLevels.Item deserializedItem in currentLevel.items)
-		{
-			// caching prefabString i.e. "phone"
-			string prefabString = deserializedItem.prefab;
-			
-			// if the prefab in the item XmlNode has not been loaded then add it to the prefabsDict dictionary,
-			if (!prefabPool.ContainsKey(prefabString))
-			{
-				// load prefab
-				GameObject prefabObject = Resources.Load (prefabsFolder + prefabString, typeof(GameObject)) as GameObject;
-				
-				// if unsuccesful, error message and jump to next in the foreach loop
-				if (prefabObject == null)
-				{
-					Debug.LogError ("Prefab \"" + prefabString + "\" does not exists.");
-					continue;
-				}
-				
-				// otherwise add to dictionary
-				prefabPool.Add (prefabString, prefabObject);
-			}
-			
-			ItemStruct item;
-			item.prefab = prefabPool[prefabString];
-			item.x 		= toFloatZeroIfNull(deserializedItem.x);
-			item.y 		= toFloatZeroIfNull(deserializedItem.y);
-			item.rot 	= toFloatZeroIfNull(deserializedItem.rot);
-			item.scale_x= toFloatOneIfNull(deserializedItem.scale_x);
-			item.scale_y= toFloatOneIfNull(deserializedItem.scale_y);
-			
-			sceneItemsList.Add (item);
-		}
-		
-		// Finally instantiate all items
-		foreach (ItemStruct item in sceneItemsList)
-		{
+        parentOfXmlItems = new GameObject(xmlItemsGOName).transform;
+    }
 
-			// TODO load height coordinate from a directory
-			GameObject newGameObject = MonoBehaviour.Instantiate(item.prefab) as GameObject;
+    public void generateItems()
+    {
+        init();
 
-			// set position
-			setPos2D (newGameObject, new Vector2(item.x, item.y));
+        DeserializedLevels.Level currentLevel = getCurLevel();
 
-			// set rotation
-			setRot2D (newGameObject, item.rot);
+        createSceneItemsList(currentLevel);
 
-			// set scale
-			newGameObject.transform.localScale = new Vector3 (item.scale_x, item.scale_y, 1);
-			
-			// set parent
-			newGameObject.transform.parent = parentOfXmlItems;
-		}
-	}
+        // Finally instantiate all items
+        instantiateItems();
+    }
 
-	// DONE, these are only helper functions below
+    private DeserializedLevels.Level getCurLevel()
+    {
+        deserializedLevels = XmlIO.LoadXml<DeserializedLevels>("Levels");
 
-	// if no value then return zero or one, otherwise convert to float
-	float toFloatZeroIfNull (string value) { return value == null ? 0 : float.Parse(value);	}
-	float toFloatOneIfNull  (string value) { return value == null ? 1 : float.Parse(value);	}
+        // if startlevel is in the XML i.e. <Developer StartLevel="3" /> then get level from there
+        // otherwise start with level 1
+        int startLevel = int.Parse(deserializedLevels.developer.startLevel);
 
-	void setPos2D(GameObject g, Vector2 pos)
-	{
-		g.transform.position = new Vector3 (
-			pos.x,
-			pos.y,
-			g.transform.position.z
-		);
-	}
+        DeserializedLevels.Level currentLevel = deserializedLevels.levels[startLevel - 1];
+        return currentLevel;
+    }
 
-	void setRot2D(GameObject g, float rot)
-	{
-		Quaternion rotation = Quaternion.identity;
-		rotation.eulerAngles = new Vector3(0, 0, rot);
-		g.transform.localRotation = rotation;
-	}
+
+    private void instantiateItems()
+    {
+        foreach (ItemStruct item in sceneItemsList)
+        {
+
+            // TODO load height coordinate from a directory
+            GameObject newGameObject = MonoBehaviour.Instantiate(item.prefab) as GameObject;
+
+            // set position
+            setPos2D(newGameObject, new Vector2(item.x, item.y));
+
+            // set rotation
+            setRot2D(newGameObject, item.rot);
+
+            // set scale
+            newGameObject.transform.localScale = new Vector3(item.scale_x, item.scale_y, 1);
+
+            // set parent
+            newGameObject.transform.parent = parentOfXmlItems;
+        }
+    }
+
+    private void createSceneItemsList(DeserializedLevels.Level currentLevel)
+    {
+        // <Item prefab="Chair" x="1" y="10" rot="90" />
+        foreach (DeserializedLevels.Item deserializedItem in currentLevel.items)
+        {
+            // caching prefabString i.e. "phone"
+            string prefabString = deserializedItem.prefab;
+
+            // if the prefab in the item XmlNode has not been loaded then add it to the prefabsDict dictionary,
+            if (!prefabPool.ContainsKey(prefabString))
+            {
+                // load prefab
+                GameObject prefabObject = Resources.Load(prefabsFolder + prefabString, typeof(GameObject)) as GameObject;
+
+                // if unsuccesful, error message and jump to next in the foreach loop
+                if (prefabObject == null)
+                {
+                    Debug.LogError("Prefab \"" + prefabString + "\" does not exists.");
+                    continue;
+                }
+
+                // otherwise add to dictionary
+                prefabPool.Add(prefabString, prefabObject);
+            }
+
+            ItemStruct item;
+            item.prefab = prefabPool[prefabString];
+            item.x = toFloatZeroIfNull(deserializedItem.x);
+            item.y = toFloatZeroIfNull(deserializedItem.y);
+            item.rot = toFloatZeroIfNull(deserializedItem.rot);
+            item.scale_x = toFloatOneIfNull(deserializedItem.scale_x);
+            item.scale_y = toFloatOneIfNull(deserializedItem.scale_y);
+
+            sceneItemsList.Add(item);
+        }
+    }
+
+    // DONE, these are only helper functions below
+
+    // if no value then return zero or one, otherwise convert to float
+    float toFloatZeroIfNull(string value) { return value == null ? 0 : float.Parse(value); }
+    float toFloatOneIfNull(string value) { return value == null ? 1 : float.Parse(value); }
+
+    void setPos2D(GameObject g, Vector2 pos)
+    {
+        g.transform.position = new Vector3(
+            pos.x,
+            pos.y,
+            g.transform.position.z
+        );
+    }
+
+    void setRot2D(GameObject g, float rot)
+    {
+        Quaternion rotation = Quaternion.identity;
+        rotation.eulerAngles = new Vector3(0, 0, rot);
+        g.transform.localRotation = rotation;
+    }
 
 }
