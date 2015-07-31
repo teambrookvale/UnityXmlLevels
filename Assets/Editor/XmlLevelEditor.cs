c7a36c379f0c0957080ea2d545a23f1a17e51c05
Pavel Kvas <kvaspasha@yandex.ru>
2015-07-31 09:55:49 +0300

    refactor XmlLevelEditor

--- Assets/Editor/XmlLevelEditor.cs
+++ Assets/Editor/XmlLevelEditor.cs
@@ -1,57 +1,65 @@
 using UnityEngine;
 using UnityEditor;
 
-public class XmlLevelEditor : EditorWindow {
+public class XmlLevelEditor : EditorWindow
+{
 
-	DeserializedLevelsLoader 		deserializedLevelsLoader;
-	DeserializedLevelsSaver  		deserializedLevelsSaver;
-	DeserializedLevelsCrossChecker	deserializedLevelsCrossChecker;
+    DeserializedLevelsLoader deserializedLevelsLoader;
+    DeserializedLevelsSaver deserializedLevelsSaver;
+    DeserializedLevelsCrossChecker deserializedLevelsCrossChecker;
 
-	[MenuItem("Window/Xml Level Editor")]
-	public static void ShowWindow()
-	{
-		//Show existing window instance. If one doesn't exist, make one.
-		EditorWindow.GetWindow(typeof(XmlLevelEditor));
-	}
-	
-	void OnGUI()
-	{
-		// create one DeserializedLevelsLoader and Saver instance
-		if (deserializedLevelsLoader 		== null) deserializedLevelsLoader 		= new DeserializedLevelsLoader();
-		if (deserializedLevelsSaver  		== null) deserializedLevelsSaver  		= new DeserializedLevelsSaver();
-		if (deserializedLevelsCrossChecker	== null) deserializedLevelsCrossChecker	= new DeserializedLevelsCrossChecker();
+    string importGOName = DeserializedLevelsLoader.xmlItemsGOName;
+    string exportGOName = DeserializedLevelsSaver.xmlItemsToExportGOName;
 
+    [MenuItem("Window/Xml Level Editor")]
+    public static void ShowWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(XmlLevelEditor));
+    }
 
-		// Import section
-		GUILayout.Label ("Import", EditorStyles.boldLabel);
-		GUILayout.Label ("Import Levels.xml into the scene");
+    void OnGUI()
+    {
+        // create DeserializedLevelsLoader, Saver and CrossChecker instances
+        init();
 
-		if (GUILayout.Button("Import Levels.xml"))
-			deserializedLevelsLoader.generateItems();
+        // Import section
+        GUILayout.Label("Import", EditorStyles.boldLabel);
+        GUILayout.Label("Import Levels.xml into the scene");
 
-
-		// Export section
-		GUILayout.Label ("Export", EditorStyles.boldLabel);
-		GUILayout.Label ("Export children of \"" + DeserializedLevelsSaver.xmlItemsToExportGOName +"\" GameObject into " + DeserializedLevelsSaver.xmlItemsToExportGOName +".xml", EditorStyles.wordWrappedLabel);
-		if (GUILayout.Button("Export " + DeserializedLevelsSaver.xmlItemsToExportGOName))
-			deserializedLevelsSaver.saveExportItems();
+        if (GUILayout.Button("Import Levels.xml"))
+            deserializedLevelsLoader.generateItems();
 
 
-		// Delete section
-		GUILayout.Label ("Delete", EditorStyles.boldLabel);
-		GUILayout.Label ("Delete " + DeserializedLevelsLoader.xmlItemsGOName + " and " + DeserializedLevelsSaver.xmlItemsToExportGOName + " GameObjects from scene", EditorStyles.wordWrappedLabel);
-		if (GUILayout.Button("Delete"))
-		{
-			DestroyImmediate(GameObject.Find (DeserializedLevelsLoader.xmlItemsGOName));
-			DestroyImmediate(GameObject.Find (DeserializedLevelsSaver.xmlItemsToExportGOName));
-		}
+        // Export section
+        GUILayout.Label("Export", EditorStyles.boldLabel);
+        GUILayout.Label("Export children of \"" + exportGOName + "\" GameObject into " + exportGOName + ".xml", EditorStyles.wordWrappedLabel);
+        if (GUILayout.Button("Export " + exportGOName))
+            deserializedLevelsSaver.saveExportItems();
 
 
-		// Cross check section
-		GUILayout.Label ("Cross Check", EditorStyles.boldLabel);
-		GUILayout.Label ("Cross check /Resources/Prefabs and Levels.xml if there are any item prefabs that exist only in one but not the other", EditorStyles.wordWrappedLabel);
-		if (GUILayout.Button("Cross Check"))
-			deserializedLevelsCrossChecker.crossCheck();
-	}
+        // Delete section
+        GUILayout.Label("Delete", EditorStyles.boldLabel);
+        GUILayout.Label("Delete " + importGOName + " and " + exportGOName + " GameObjects from scene", EditorStyles.wordWrappedLabel);
+        if (GUILayout.Button("Delete"))
+        {
+            DestroyImmediate(GameObject.Find(importGOName));
+            DestroyImmediate(GameObject.Find(exportGOName));
+        }
+
+
+        // Cross check section
+        GUILayout.Label("Cross Check", EditorStyles.boldLabel);
+        GUILayout.Label("Cross check /Resources/Prefabs and Levels.xml if there are any item prefabs that exist only in one but not the other", EditorStyles.wordWrappedLabel);
+        if (GUILayout.Button("Cross Check"))
+            deserializedLevelsCrossChecker.crossCheck();
+    }
+
+    private void init()
+    {
+        if (deserializedLevelsLoader == null) deserializedLevelsLoader = new DeserializedLevelsLoader();
+        if (deserializedLevelsSaver == null) deserializedLevelsSaver = new DeserializedLevelsSaver();
+        if (deserializedLevelsCrossChecker == null) deserializedLevelsCrossChecker = new DeserializedLevelsCrossChecker();
+    }
 
 }
