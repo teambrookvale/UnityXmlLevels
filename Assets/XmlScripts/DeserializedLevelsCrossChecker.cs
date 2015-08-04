5a90e75aa7492fd7eff5bdf6bd2060884c2a4bb6
Pavel Kvas <kvaspasha@yandex.ru>
2015-08-04 17:28:09 +0300

    refactor DeserializedLevelsCrossChecker
    change container type from List to HashSet

--- Assets/XmlScripts/DeserializedLevelsCrossChecker.cs
+++ Assets/XmlScripts/DeserializedLevelsCrossChecker.cs
@@ -8,32 +8,30 @@ using System.Linq;
 
 public class DeserializedLevelsCrossChecker
 {
-
-
     // cross check /Resources/Prefabs and Levels.xml if there are any item prefabs that exist only in one but not the other
     public void crossCheck()
     {
-        // create a list of /Resources/Prefabs for resources and Levels.Xml
-        List<string> resPrefabList = new List<string>();
-        List<string> xmlPrefabList = new List<string>();
+        // create a two sets of prefabs
+        HashSet<string> resPrefabSet = new HashSet<string>();
+        HashSet<string> xmlPrefabSet = new HashSet<string>();
 
         // Get prefabs from Levels.xml
-        getLevelPrefabs(xmlPrefabList);
+        getLevelPrefabs(xmlPrefabSet);
 
         // Get prefabs from the /Resources/Prefabs folder
-        getResPrefabs(resPrefabList);
+        getResPrefabs(resPrefabSet);
 
         // Cross checks
-        foreach (string prefab in xmlPrefabList.Except(resPrefabList).ToList())
+        foreach (string prefab in xmlPrefabSet.Except(resPrefabSet).ToList())
             Debug.LogError(prefab + " is missing in the /Resorces/Prefabs folder but used in Levels.xml");
 
-        foreach (string prefab in resPrefabList.Except(xmlPrefabList).ToList())
+        foreach (string prefab in resPrefabSet.Except(xmlPrefabSet).ToList())
             Debug.Log(prefab + " exists in the /Resorces/Prefabs folder but not used in Levels.xml");
 
         Debug.Log("Cross Check Done");
     }
 
-    private static void getResPrefabs(List<string> resPrefabList)
+    private static void getResPrefabs(HashSet<string> resPrefabList)
     {
         // get all child items in the /Resources/Prefabs folder
         DirectoryInfo dir = new DirectoryInfo("Assets/Resources/Prefabs");
@@ -42,15 +40,21 @@ public class DeserializedLevelsCrossChecker
 
         // Add each prefab's file name to prefabList and truncate the .prefab extension from the end
         foreach (FileInfo fileInfo in fileInfos)
+        {
             resPrefabList.Add(fileInfo.Name.Substring(0, fileInfo.Name.Length - ".prefab".Length));
+        }
     }
 
-    private static void getLevelPrefabs(List<string> xmlPrefabList)
+    private static void getLevelPrefabs(HashSet<string> xmlPrefabSet)
     {
         DeserializedLevels deserializedLevels = XmlIO.LoadXml<DeserializedLevels>("Levels");
         foreach (DeserializedLevels.Level level in deserializedLevels.levels)
+        {
             foreach (DeserializedLevels.Item item in level.items)
-                if (!xmlPrefabList.Contains(item.prefab))   //TODO Possible bottleneck
-                    xmlPrefabList.Add(item.prefab);
+            {
+               xmlPrefabSet.Add(item.prefab);
+            }
+        }
     }
+
 }
