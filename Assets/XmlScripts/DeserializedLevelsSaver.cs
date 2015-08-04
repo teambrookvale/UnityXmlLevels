db82cd22b0a27aff5c2a7c0c4074b0168870efe8
Pavel Kvas <kvaspasha@yandex.ru>
2015-08-04 12:55:03 +0300

    DeserializedLevelsSaver refactoring

--- Assets/XmlScripts/DeserializedLevelsSaver.cs
+++ Assets/XmlScripts/DeserializedLevelsSaver.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using System.Xml;
 
 public class DeserializedLevelsSaver
@@ -30,31 +30,21 @@ public class DeserializedLevelsSaver
             return;
         }
 
-        DeserializedLevels.Level levelXml = new DeserializedLevels.Level();
+        //create list of items
+        List<DeserializedLevels.Item> itemList = new List<DeserializedLevels.Item>();
 
-        int n = 0;
-        // count number of children skipping sub-items
-        foreach (Transform item in xmlItemsToExportGOchildren)  //TODO replace two xmlItemsToExportGOchildren traversals with one
-            if (item.parent == xmlItemsToExportGO.transform) n++;
-
-        // the items array should have that many elements
-        levelXml.items = new DeserializedLevels.Item[n];
-
-        // use i for counting items, it would be equal (one more to be precise) to n at the end of the cycle
-        int i = 0;
-
-        // cycle through the children again and add them to items
-        foreach (Transform item in xmlItemsToExportGOchildren)
+        foreach (Transform item in xmlItemsToExportGOchildren)  
         {
-            // skip sub-items
-            if (item.parent != xmlItemsToExportGO.transform) continue;
-
-            levelXml.items[i] = new DeserializedLevels.Item(item);           
-
-            // increase i for the next cycle
-            i++;
+            if (item.parent == xmlItemsToExportGO.transform)
+            {
+                itemList.Add(new DeserializedLevels.Item(item));
+            }
         }
-
+        
+        //copy list of items to the raw array
+        DeserializedLevels.Level levelXml = new DeserializedLevels.Level();        
+        levelXml.items = new DeserializedLevels.Item[itemList.Count];
+        itemList.CopyTo(levelXml.items);
 
         // Export just one level
         DeserializedLevels levelsXmlToExport = new DeserializedLevels();
